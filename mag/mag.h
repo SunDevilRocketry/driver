b23f790487f707a1c8cbc8f8d0f6b65d1a074f7e
Dexx Stexx <drewpad@icloud.com>
2026-09-21 16:01:33 -0700

    Write header file description

---
+++ mag/mag.h
@@ -0,0 +1,20 @@
+/*******************************************************************************
+*
+* FILE: 
+* 		mag.h
+*
+* DESCRIPTION: 
+* 		Contains API functions for A0010 (rev3 Flight Computer) magnetometer
+*
+* COPYRIGHT:                                                                   
+*       Copyright (c) 2026 Sun Devil Rocketry.                                 
+*       All rights reserved.                                                   
+*                                                                              
+*       This software is licensed under terms that can be found in the LICENSE 
+*       file in the root directory of this software component.                 
+*       If no LICENSE file comes with this software, it is covered under the   
+*       BSD-3-Clause.                                                          
+*                                                                              
+*       https://opensource.org/license/bsd-3-clause          
+*
+*******************************************************************************/
